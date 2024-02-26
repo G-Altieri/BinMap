ed296e01842ef1c5c30b32be25395594686a84f4
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2024-02-26 20:36:23 +0100

    add frame list and home

--- app/src/main/java/it/gteam/app/binmap/MainActivity.java
+++ app/src/main/java/it/gteam/app/binmap/MainActivity.java
@@ -1,14 +1,28 @@
 package it.gteam.app.binmap;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import it.gteam.app.binmap.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater()); //posso fare getLayoutInflater perché sono in un'activity e non in un frammento
+
+        setContentView(binding.getRoot());
+
+        // questo fa funzionare il bottomNavView tramite il click
+        NavHostFragment fragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerViewMainAnctivity);
+        if (fragment != null) {
+            NavController controller = fragment.getNavController();
+            NavigationUI.setupWithNavController(binding.bottomNavigationView, controller);
+        }
     }
 }
